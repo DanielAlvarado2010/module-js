83b9c0d1ddc2bf3d60de504b43b5ed902e9b0774
Daniel_Alvarado <danielalvarado1498@gmail.com>
2021-10-20 22:14:02 -0500

    Ejercicio 1 DOM

--- Ejercicio1/js/script.js
+++ Ejercicio1/js/script.js
@@ -1,245 +1,28 @@
-// const person = {
-//   name:'ivan',
-//   paellisdo:'',
-//   saludar: function (params) {
+console.log(document)
 
-//   }
-// };
+const h1 = document.createElement("h1")
+const text = document.createTextNode("Hola Koders")
 
-// console.log(person.saludar())
+h1.appendChild(text)
 
-// const number = [1,2,3]
-// number[1]
+document.body.appendChild(h1)
 
-// console.log(number)
+console.log(h1)
 
-// const test = new String('este es un string')
-// console.log(test)
+const personas =  [
+    "arnold",
+    "gerald",
+    "gelga"
+]
 
-function Persona(name, lastName) {
-    this.name = name;
-    this.lastName = lastName;
-  }
-  
-  // const otra = () => {
-  //   console.log(this);
-  // };
-  
-  // console.log(otra())
-  
-  const person1 = new Persona('ivan', 'diaz')
-  
-  const person2 = new Persona("German", "Martinez");
-  
-  Persona.prototype.saludar = function() {
-    console.log(`hola soy ${this.name}`);
-  };
-  // console.log(person1)
-  console.log([person1, person2]);
-  
-  
-  
-  const koders = [
-    {
-      name: "Harold",
-      lastName: "López",
-      birthday: "1996/06/24",
-      generation: 9,
-      bootcamp: "JavaScript",
-      scores: [
-        {
-          module: "Frontend",
-          score: 90
-        },
-        {
-          module: "Backend",
-          score: 80
-        },
-        {
-          module: "Cloud",
-          score: 80
-        }
-      ]
-    },
-  
-    {
-      name: "Arnold",
-      lastName: "Osborn",
-      birthday: "1998/05/12",
-      generation: 1,
-      bootcamp: "Python",
-      scores: [
-        {
-          module: "Frontend",
-          score: 90
-        },
-        {
-          module: "Backend",
-          score: 100
-        },
-        {
-          module: "Cloud",
-          score: 100
-        }
-      ]
-    },
-    {
-      name: "Peter",
-      lastName: "Parker",
-      birthday: "1994/10/26",
-      generation: 10,
-      bootcamp: "JavaScript",
-      scores: [
-        {
-          module: "Frontend",
-          score: 100
-        },
-        {
-          module: "Backend",
-          score: 95
-        },
-        {
-          module: "Cloud",
-          score: 80
-        }
-      ]
-    },
-    {
-      name: "Homer",
-      lastName: "Simpson",
-      birthday: "1996/06/24",
-      generation: 1,
-      bootcamp: "Python",
-      scores: [
-        {
-          module: "Frontend",
-          score: 98
-        },
-        {
-          module: "Backend",
-          score: 100
-        },
-        {
-          module: "Cloud",
-          score: 70
-        }
-      ]
-    },
-    {
-      name: "Ren",
-      lastName: "López",
-      birthday: "1996/06/24",
-      generation: 9,
-      bootcamp: "JavaScript",
-      scores: [
-        {
-          module: "Frontend",
-          score: 80
-        },
-        {
-          module: "Backend",
-          score: 90
-        },
-        {
-          module: "Cloud",
-          score: 100
-        }
-      ]
-    }
-  ];
-  console.log(koders)
-  
-  /*
-  Ejercicio en clase:
-  Teniendo la siguiente coleccion de Koders generar una coleccion de objetos de tipo Koder. Agregando además las siguientes funciones:
-  
-  Prototipo de tipo koder
-  
-  Obtener la edad a partir de la fecha de nacimiento
-  
-  Obtener promedio de sus scores
-  
-  ---
-  Colección de Koder que pertenezcan a JavaScript
-  
-  Colección de Koder que pertenezcan a Python
-  
-  {
-    kavascrip: [
-      koder1
-    ],
-    python: [
-      
-    ]
-  
-  }
-  */
-
-//   function getAge (age) {
-//     const currentYear = new Date().getFullYear()
-//     age = parseInt(birthday.slice(0,4))
-//     console.log(age);
-//     return currentYear - parseInt(age.slice(0,4))
-//   }
-  
-//   function getPromedio (scores) {
-//     return scores.reduce((acum, next) => {
-//       return acum + next.score / scores.length
-//     },0)
-//   }
-  
-//   function Koder (name, lastName, birthday, generation, bootcamp, promedio) {
-//       this.name = name
-//       this.lastName = lastName
-//       this.birthday = birthday
-//       this.age = getAge(birthday)
-//       this.generation = generation
-//       this.bootcamp = bootcamp
-//       this.promedio = parseInt(getPromedio(promedio).toFixed(2))
-//   }
-  
-//   const newKoders = koders.map(function(koder) {
-//       return new Koder(koder.name, koder.lastName, koder.birthday, koder.generation, koder.bootcamp, koder.scores)
-//   })
-
-//   class Persona {
-//       constructor (name, lastName) {
-//           this.name = name;
-//           this.lastName = lastName;
-//       }
-//   }
-
-// Pasos:
-// Iterar el arreglo con map
-// generar una funcion constructora
-//por cada elemento vamos a crear un objeto tipo Koder
-//retornar nuestro nuevo objeto tipo Koder
+const ul = document.createElement("ul")
 
 
-class Alumno {
-    constructor(name, lastName, bootcamp, promedio) {
-    this.name = name;
-    this.lastName = lastName;
-    this.bootcamp = bootcamp;
-    this.promedio = promedio;
-    }
-}
+personas.forEach ((persona)=> {
+    const li = document.createElement("li");
+    const text = document.createTextNode(persona);   
+    li.appendChild(text);
+    ul.appendChild(li);
+})
 
-const getAverage = (calificaciones) => {
-    const promedio = calificaciones.reduce ((accum, currentValue) => {
-        return accum + currentValue.score / calificaciones.length; 
-    }, 0)
-    return promedio;
-}
-
-function koderFactory (KodersArray) {
-    const newKoders = KodersArray.map ((koder) => {
-        const promedio = getAverage(koder.scores);
-        const koderType = new Alumno(koder.name , koder.lastName, koder.bootcamp, promedio)
-        return koderType;
-    })
-    return newKoders;
-}
-
-console.log(koderFactory(koders))
-
+document.body.appendChild(ul)
--- Ejercicio2/js/script.js
+++ Ejercicio2/js/script.js
@@ -1,18 +1,153 @@
-class Persona {
-    constructor (nombre, lastName) {
-        this.nombre = nombre;
-        this.lastName = lastName;
-    }
-}
+const mentorsArray = [
+    {
+        name:"Ivan Diaz",
+        scores:[
+            {
+                signature:"HTML",
+                score:8
+            },
+            {
+                signature:"CSS",
+                score:10
+            },
+            {
+                signature:"JS",
+                score:8
+            },
+            {
+                signature:"ReactJS",
+                score:8
+            }
+        ]
+    },
+    {
+        name:"Alan Medina",
+        scores:[
+            {
+                signature:"HTML",
+                score:8
+            },
+            {
+                signature:"CSS",
+                score:7
+            },
+            {
+                signature:"JS",
+                score:10
+            },
+            {
+                signature:"ReactJS",
+                score:10
+            }
+        ]
+    },
+    {
+        name:"Elvira Camarillo",
+        scores:[
+            {
+                signature:"HTML",
+                score:9
+            },
+            {
+                signature:"CSS",
+                score:9
+            },
+            {
+                signature:"JS",
+                score:10
+            },
+            {
+                signature:"ReactJS",
+                score:9
+            }
+        ]
+    },
+    {
+        name:"Alejandra Paez",
+        scores:[
+            {
+                signature:"HTML",
+                score:10
+            },
+            {
+                signature:"CSS",
+                score:10
+            },
+            {
+                signature:"JS",
+                score:10
+            },
+            {
+                signature:"ReactJS",
+               score:10
+            },
+        ]
+    },
+  ]
 
-class Koder extends Persona {
-    constructor (nombre, lastName,bootcamp, generation) {
-        super(nombre, lastName)
-        this.bootcamp = bootcamp;
-        this.generation = generation;
-    }
-}
 
-const Koder1 = new Koder("Daniel", "Alejandro", "Javascript", "14")
+  const table = document.createElement("table") 
+  const thead = document.createElement("thead")
+  const tbody = document.createElement("tbody")
+  const tr1 = document.createElement("tr")
+  const tr2 = document.createElement("tr")
+  const tr3 = document.createElement("tr")
+  const tr4 = document.createElement("tr")
+  const tr5 = document.createElement("tr")
+  const th = document.createElement("th")
 
-console.log(Koder1)
+  const mentor = document.createTextNode(" Mentor ")
+  const signature1 = document.createTextNode(" HTML ")
+  const signature2 = document.createTextNode(" CSS ")
+  const signature3 = document.createTextNode(" JS ")
+  const signature4 = document.createTextNode(" REACT ")
+  const promedio = document.createTextNode(" PROMEDIO ")
+
+
+  th.appendChild(mentor)
+  th.appendChild(signature1)
+  th.appendChild(signature2)
+  th.appendChild(signature3)
+  th.appendChild(signature4)
+  th.appendChild(promedio)
+  
+
+  tr1.appendChild(th)
+  thead.appendChild(tr1)
+  table.appendChild(thead)
+
+  document.body.appendChild(table)
+
+  const Mentor1 = document.createTextNode(" Ivan Diaz ")
+  const Mentor2 = document.createTextNode(" Alan Medina")
+  const Mentor3 = document.createTextNode(" Elvira Camarillo ")
+  const Mentor4 = document.createTextNode(" Alejandra Paez ")
+
+
+  
+  tr2.appendChild(Mentor1)
+  tr3.appendChild(Mentor2)
+  tr4.appendChild(Mentor3)
+  tr5.appendChild(Mentor4)
+
+  tbody.appendChild(tr1)
+  tbody.appendChild(tr2)
+  tbody.appendChild(tr3)
+  tbody.appendChild(tr4)
+  tbody.appendChild(tr5)
+
+
+  table.appendChild(tbody)
+
+  document.body.appendChild(table)
+
+
+
+
+
+
+  
+
+
+
+  
