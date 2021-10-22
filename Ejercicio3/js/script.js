3d7ed71af8613872a521a535a35719da5c4bffbf
Daniel_Alvarado <danielalvarado1498@gmail.com>
2021-10-21 19:21:05 -0500

    Ejercicio DOM

--- Ejercicio3/js/script.js
+++ Ejercicio3/js/script.js
@@ -1,159 +1,138 @@
-const koders = [
+const mentorsArray = [
     {
-      name: "Harold",
-      lastName: "López",
-      birthday: "1996/06/24",
-      generation: 9,
-      bootcamp: "JavaScript",
-      scores: [
-        {
-          module: "Frontend",
-          score: 90
-        },
-        {
-          module: "Backend",
-          score: 80
-        },
-        {
-          module: "Cloud",
-          score: 80
-        }
-      ]
-    },
-  
-    {
-      name: "Arnold",
-      lastName: "Osborn",
-      birthday: "1998/05/12",
-      generation: 1,
-      bootcamp: "Python",
-      scores: [
-        {
-          module: "Frontend",
-          score: 90
-        },
-        {
-          module: "Backend",
-          score: 100
-        },
-        {
-          module: "Cloud",
-          score: 100
-        }
-      ]
+        name:"Ivan Diaz",
+        scores:[
+            {
+                signature:"HTML",
+                score:8
+            },
+            {
+                signature:"CSS",
+                score:10
+            },
+            {
+                signature:"JS",
+                score:8
+            },
+            {
+                signature:"ReactJS",
+                score:8
+            }
+        ]
     },
     {
-      name: "Peter",
-      lastName: "Parker",
-      birthday: "1994/10/26",
-      generation: 10,
-      bootcamp: "JavaScript",
-      scores: [
-        {
-          module: "Frontend",
-          score: 100
-        },
-        {
-          module: "Backend",
-          score: 95
-        },
-        {
-          module: "Cloud",
-          score: 80
-        }
-      ]
+        name:"Alan Medina",
+        scores:[
+            {
+                signature:"HTML",
+                score:8
+            },
+            {
+                signature:"CSS",
+                score:7
+            },
+            {
+                signature:"JS",
+                score:10
+            },
+            {
+                signature:"ReactJS",
+                score:10
+            }
+        ]
     },
     {
-      name: "Homer",
-      lastName: "Simpson",
-      birthday: "1996/06/24",
-      generation: 1,
-      bootcamp: "Python",
-      scores: [
-        {
-          module: "Frontend",
-          score: 98
-        },
-        {
-          module: "Backend",
-          score: 100
-        },
-        {
-          module: "Cloud",
-          score: 70
-        }
-      ]
+        name:"Elvira Camarillo",
+        scores:[
+            {
+                signature:"HTML",
+                score:9
+            },
+            {
+                signature:"CSS",
+                score:9
+            },
+            {
+                signature:"JS",
+                score:10
+            },
+            {
+                signature:"ReactJS",
+                score:9
+            }
+        ]
     },
     {
-      name: "Ren",
-      lastName: "López",
-      birthday: "1996/06/24",
-      generation: 9,
-      bootcamp: "JavaScript",
-      scores: [
-        {
-          module: "Frontend",
-          score: 80
-        },
-        {
-          module: "Backend",
-          score: 90
-        },
-        {
-          module: "Cloud",
-          score: 100
-        }
-      ]
-    }
-  ]; 
- 
- function getAge (age) {
-    const currentYear = new Date().getFullYear()
-    age = age.slice(0,4)
-    console.log(age);
-    return currentYear - parseInt(age.slice(0,4))
-  }
-  
-  function getPromedio (scores) {
-    return scores.reduce((acum, next) => {
-      return acum + next.score / scores.length
-    },0)
-  }
-  
-  function Koder (name, lastName, birthday, generation, bootcamp, promedio) {
-      this.name = name
-      this.lastName = lastName
-      this.birthday = birthday
-      this.age = getAge(birthday)
-      this.generation = generation
-      this.bootcamp = bootcamp
-      this.promedio = parseInt(getPromedio(promedio).toFixed(2))
-  }
-  
-  const newKoders = koders.map(function(koder) {
-      return new Koder(koder.name, koder.lastName, koder.birthday, koder.generation, koder.bootcamp, koder.scores)
-  })
+        name:"Alejandra Paez",
+        scores:[
+            {
+                signature:"HTML",
+                score:10
+            },
+            {
+                signature:"CSS",
+                score:10
+            },
+            {
+                signature:"JS",
+                score:10
+            },
+            {
+                signature:"ReactJS",
+               score:10
+            },
+        ]
+    },
+  ]
 
-  class koder {
-      constructor (name, lastName, age, generation, bootcamp, average){
-        this.name=name;
-        this.lastName=lastName;
-        this.age =getAge(age);
-        this.generation =generation;
-        this.bootcamp = bootcamp;
-        this.promedio = parseInt(getPromedio(promedio).toFixed(2))
-      }
-  }
+//Crea tabla, encabezados fijos
+const table = document.createElement('table');
+const header = document.createElement('tr');
+const thMentor = document.createElement('th');
+const textMentor = document.createTextNode('MENTOR');
+thMentor.appendChild(textMentor);
+header.appendChild(thMentor);
+//Llena encabezados con info de scores.signature
+const headerBySignatures = mentorsArray[0].scores.forEach(mentor => {
+    const thBySignature = document.createElement('th');
+    const txtBySignature = document.createTextNode(mentor.signature);
+    thBySignature.appendChild(txtBySignature);
+    header.appendChild(thBySignature);
+});
+//Pone encabezado fijo 'Average'
+const thAverage = document.createElement('th')
+const headerAverage = document.createTextNode('Average');
+thAverage.appendChild(headerAverage);
+header.appendChild(thAverage)
+table.appendChild(header);
 
-//   const newKoders = koders.map((koder) => {
-//       return new Koder(koder.name, koder.lastName, koder.birthday, koder.generation, koder. bootcamp, koder.scores)
-//   })
+//Función que recorre primero por mentor.name y luego por signature.scores
+const inspectMentor = () =>{
+    mentorsArray.forEach(mentor=>{
+        const trMentor = document.createElement('tr');
+        const tdMentor = document.createElement('th');
+        const nameMentor = document.createTextNode(mentor.name);
+        tdMentor.appendChild(nameMentor);
+        trMentor.appendChild(tdMentor)
 
-  console.log(newKoders)
-
-  const JavaScriptKoders = newKoders.filter((koder) => koder.bootcamp === "JavaScript")
-
-  const pythonKoders = newKoders.filter((koder) => koder.bootcamp === "Python")
-
-  console.log(JavaScriptKoders)
-  console.log(pythonKoders)
+        const inspectSignatures = mentor.scores.map(signature=>{
+            const tdSignature = document.createElement('td');
+            const scoreBySignature = document.createTextNode(signature.score);
+            tdSignature.appendChild(scoreBySignature);
+            const arrayScoresByMentor = signature.score;
+            trMentor.appendChild(tdSignature);
+            return arrayScoresByMentor;
+        });
+        //Por fuera se obtiene el average con el arreglo que devuelve el map
+        const averageByMentor = inspectSignatures.reduce((acum,item)=>acum+item/inspectSignatures.length,0)
+        const thAverage = document.createElement('th')
+        const textAverage = document.createTextNode(averageByMentor)
+        thAverage.appendChild(textAverage)
+        trMentor.appendChild(thAverage)
+        
+        table.appendChild(trMentor)
+    })
+}
+inspectMentor();
+console.log(table);
+document.body.appendChild(table);
